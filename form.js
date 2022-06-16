bdd338d70621146d886e2b3325c6d06f6dc35a1d
levink1900 <105488343+levink1900@users.noreply.github.com>
2022-06-16 12:13:23 +0200

    Add files via upload

---
+++ form.js
@@ -0,0 +1,31 @@
+const email = document.getElementById("email")
+const firstname = document.getElementById("firstname")
+const lastname = document.getElementById("lastname") 
+const submit = document.getElementById("submit")
+submit.disabled = true
+
+const validate = () => {
+    if (email.value == "") {
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+}
+
+email.addEventListener("keyup", (event) => {
+    validate()
+})
+
+
+submit.addEventListener("click", async (event) => {
+    event.preventDefault()
+    const result = await databaseClient.insertInto("users", ["email","firstname","lastname"], [email.value,firstname.value,lastname.value])
+    if (result.error) {
+        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+    }
+    else {
+        // Weiterleitung auf die Game Page  
+        location.href = "./game.html"
+    }
+
+})
