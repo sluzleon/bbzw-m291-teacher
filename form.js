6b67b58105ddf5e333f8b3483a78f5a663b7d2e4
levink1900 <105488343+levink1900@users.noreply.github.com>
2022-06-17 15:51:32 +0200

    Update form.js

--- form.js
+++ form.js
@@ -2,14 +2,22 @@ const email = document.getElementById("email")
 const firstname = document.getElementById("firstname")
 const lastname = document.getElementById("lastname") 
 const submit = document.getElementById("submit")
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 submit.disabled = true
 
+function validateEmail(email) 
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+ }
 
 const validate = () => {
-    if (email.value =="" || firstname.value == "" || lastname.value == "") {
+    if (email.value == "" || firstname.value == "" || lastname.value == "") {
       submit.disabled = true
-    } else {
+    } else if (validateEmail (email.value) && !(firstname.value == "" && !(lastname.value == ""))) { 
       submit.disabled = false
+    } else {
+      submit.disabled = true
     }
   }
 
@@ -30,7 +38,7 @@ submit.addEventListener("click", async (event) => {
         alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
     }
     else {
-        // Weiterleitung auf die Game Page  
+        // Weiterleitung auf unsere Game Page  
         location.href = "./Game.html"
     }
 
